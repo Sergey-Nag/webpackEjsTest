da49182f99c0e390111145dac31e5f3eba0fb52f
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-01-20 23:51:11 +0200

    вроде работает

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './scss/style.scss';
+import index from "./index.ejs";
+import comp from "./comp/component.ejs";
+// import './aaa.ejs';
+
+// index({title: "HWLLO WORLD",comp: comp});
+// var component = require('ejs-compiled-loader!./comp/component.ejs');
+// var template = require("ejs-compiled!./index.ejs");
+// => returns the template function compiled with ejs templating engine.
+// component({title:'hello'});
+// And then use it somewhere in your code
+// 
+
+console.log("HELLO")
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const WatchExternalFilesPlugin = require('webpack-watch-files-plugin');
+const pagesData = require("./pages-data.json");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.ejs$/i,
+        loader: "ejs-loader",
+        options: {
+          esModule: false
+        }
+      },
+    ],
+  },
+  mode: "development",
+  entry: {
+    index: path.resolve(__dirname, "src", "index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "js/index.js",
+  },
+  watch: true,
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      // templateParameters: pagesData,
+      template: path.resolve(__dirname, "src", "index.ejs"),
+      filename: path.resolve(__dirname, "build", "index.html"),
+    }),
+    //   new webpack.ProvidePlugin({
+    //     _: "underscore"
+    // })
+  ],
+
+};
